060c7c66a34eae555a910cd2d8264563d0066400
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-08-15 00:15:39 +0900

    WIP

--- Assets/Player/PlayerMove.cs
+++ Assets/Player/PlayerMove.cs
@@ -6,10 +6,20 @@ namespace Player
     {
         private int _direction = 8;
         [SerializeField] private int reqDirection = 8;
+        private Rigidbody2D rb2D;
+        public float moveSpeed;
+        private void Start()
+        {
+            moveSpeed = 1;
+            rb2D = GetComponent<Rigidbody2D>();
+        }
         private void Update()
         {
+            /*
             new Networking.Post<Void>("/set-direction", null).AddParam("direction", reqDirection + "").Build();
             new Networking.Get<DirectionResponses>("/get-direction").OnResponse(dr => _direction = dr.direction).OnError((_) => _direction = 8).Build();
+            */
+            _direction = reqDirection;
             Move();
         }
 
@@ -20,17 +30,20 @@ namespace Player
             {
                 case 0:
                     // UP
+                    rb2D.velocity = new Vector2(0f, moveSpeed);
                     break;
                 case 1:
                     // UP-RIGHT
                     break;
                 case 2:
                     // RIGHT
+                    rb2D.velocity = new Vector2(moveSpeed, 0f);
                     break;
                 case 3:
                     // DOWN-RIGHT
                     break;
                 case 4:
+                    rb2D.velocity = new Vector2(0f, -1*moveSpeed);
                     // DOWN
                     break;
                 case 5:
@@ -38,12 +51,14 @@ namespace Player
                     break;
                 case 6:
                     // LEFT
+                    rb2D.velocity = new Vector2(-1*moveSpeed, 0f);
                     break;
                 case 7:
                     // UP_LEFT
                     break;
                 case 8:
                     // STAY
+                    rb2D.velocity = new Vector2(0f, 0f);
                     break;
             }
         }
